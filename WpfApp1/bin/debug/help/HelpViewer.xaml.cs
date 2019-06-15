90a2522362b27ab3f3f5350b9f978ca725e460a7
artbluu <43496798+artbluu@users.noreply.github.com>
2019-06-15 16:14:03 +0200

    Add files via upload

---
+++ WpfApp1/bin/debug/help/HelpViewer.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Help
+{
+    /// <summary>
+    /// Interaction logic for HelpViewer.xaml
+    /// </summary>
+    public partial class HelpViewer : Window
+    {
+        //private JavaScriptControlHelper ch;
+        public HelpViewer(string key)
+        {
+            InitializeComponent();
+            string curDir = Directory.GetCurrentDirectory();
+            string path = String.Format("{0}/Help/{1}.html", curDir, key);
+            if (!File.Exists(path))
+            {
+                key = "error";
+            }
+            Uri u = new Uri(String.Format("file:///{0}/Help/{1}.html", curDir, key));
+            //ch = new JavaScriptControlHelper(originator);
+            //wbHelp.ObjectForScripting = ch;
+            wbHelp.Navigate(u);
+        }
+    }
+}
